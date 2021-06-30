5259785263975bb0c3a8f8ea813286fb324a8591
Abhishek528-prog <84911888+Abhishek528-prog@users.noreply.github.com>
2021-06-30 22:45:56 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -1,54 +1,122 @@
-  #include<string.h>
-#include<math.h>
-#include<stdio.h>
-#include<cs50.h>
-#include<ctype.h>
-string ari(string a)
+  #include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <cs50.h>
+#include <math.h>
+#include <ctype.h>
+
+string ari(string s)
 {
-float score;
-int c=0,w=0,s=0;
-for(int i=0;a[i]!='\0';++i)
-{
-if(isalnum(a[i]))
-c++;
-if(a[i]==' ')
-w++;
-if((a[i]=='.')||(a[i]=='!')||(a[i]=='?'))
-s++;
-}
-score=(4.71*(float)c/(float)w)+(0.5*(float)w/(float)s)-21.43;
-score=(int) ceil(score);
-if(score==1)
-return "Kindergarten";
- else if(score==2) 
-return "First/Second Grade";
-else if(score==3)
-return "Third Grade";
- else if(score==4)
-return "Fourth Grade";
-else if(score==5)
-return "Fifth Grade";
-  else if(score==6)
-return "Sixth Grade";
-      else if(score==7)
-return "Seventh Grade";
-       else if(score==8)
-return "Eighth Grade";
-     else if(score==9)
-return "Ninth Grade";
-     else if(score==10)
-return "Tenth Grade";
-      else if(score==11)
-return "Eleventh Grade";
-       else if(score==12)
-return "Twelfth grade";
-else if(score==13)
-return "College student";
-     else
-return "Professor";
-       
+ int i;
+ float cha=0.000000;
+ float words=0.000000;
+ float sentence=0.000000;
+ for(i=0;s[i]!='\0';i++)
+ {
+  if(isalnum(s[i]))
+  {
+    ++cha;
+  }
  }
 
+  for(i=0;s[i]!='\0';i++)
+ {
+  if(s[i]=='.' || s[i]=='?' || s[i]=='!')
+  {
+    ++sentence;
+  }
+ }
+
+  for(i=0;s[i]!='\0';i++)
+ {
+  if(s[i]==' ')
+  {
+    ++words;
+  }
+ }
+
+ 
+ float result;
+ result= (4.71*cha/words) + (0.5*words/sentence) - 21.43;
+ int x;
+ string w;
+ x=ceil(result);
+ if(x==1)
+ {
+  w= "Kindergarden";
+  return w;
+ }
+ if(x==2)
+ {
+  w="First/Second Grade";
+  return w;
+ }
+ if(x==3)
+ {
+  w="Third Grade";
+  return w;
+ }
+ if(x==4)
+ {
+ w="Fourth Grade";
+ return w;
 }
+ if(x==5)
+  {
+ w="Fifth Grade";
+ return w;
+}
+ if(x==6)
+  {
+ w="Sixth Grade";
+ return w;
+}
+ if(x==7)
+  {
+ w="Seventh Grade";
+ return w;
+}
+ if(x==8)
+  {
+ w="Eigth Grade";
+ return w;
+}
+ if(x==9)
+ {
+ w="Ninth Grade";
+ return w;
+}
+ if(x==10)
+  {
+ w="Tenth Grade";
+ return w;
+}
+ if(x==11)
+  {
+ w="Eleventh Grade";
+ return w;
+}
+ if(x==12)
+ {
+ w="Twelfth Grade";
+ return w;
+}
+ if(x==13)
+ {
+ w="College student";
+ return w;
+}
+ if(x==14)
+ {
+ w="Professor";
+ return w;
+}
+ 
+ 
+
+
+}
+
+
 
 
